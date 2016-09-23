3e87ac30420ecacf2c7fba6703363f9a52fbf637
fa2m-alex <alex.fa2m@yandex.ua>
2016-09-23 15:08:31 +0300

    Initial

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Dictionary
+{
+    class Dictionary
+    {
+        private string filesPath;
+        string[] files;
+        StreamWriter saver;
+        List<string> collection;
+
+        public Dictionary(string filesPath)
+        {
+            this.filesPath = filesPath;
+            files = Directory.GetFiles(filesPath);
+            collection = new List<string>();
+        }
+
+        public void SaveDictionary(string path)
+        {
+            saver = new StreamWriter(path);
+
+            foreach (var file in files)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(file);
+
+                XmlNodeList elemList = doc.GetElementsByTagName("p");
+
+                foreach (XmlNode elem in elemList)
+                {
+                    foreach (var word in GetWords(elem.InnerText))
+                    {
+                        collection.Add(word.ToLower());
+                    }
+                }
+            }
+
+            collection = collection.Distinct().ToList();
+            collection.Sort();
+
+            foreach (var str in collection)
+            {
+                saver.WriteLine(str);
+            }
+
+            saver.Close();
+
+            Console.WriteLine("Done");
+
+        }
+
+        private string[] GetWords(string input)
+        {
+            MatchCollection matches = Regex.Matches(input, @"\w+");
+
+            var words = from m in matches.Cast<Match>()
+                        where !string.IsNullOrEmpty(m.Value)
+                        select m.Value;
+
+            return words.ToArray();
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        const string FILES_PATH = @"C:\Users\Alex\Desktop\Books";
+        const string SAVE_DESTINATION = @"C:\Users\Alex\Desktop\dictionary.txt";
+
+        static void Main(string[] args)
+        {
+            Dictionary d = new Dictionary(FILES_PATH);
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            d.SaveDictionary(SAVE_DESTINATION);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine(elapsedMs / 1000.0 + "s");
+        }
+    }
+}
